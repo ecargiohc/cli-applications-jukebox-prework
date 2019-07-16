0293460341379844555c0eeab5425e213e8fc5c5
Grace Colette <ilovegrace09@gmail.com>
2019-07-16 01:03:50 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,49 @@
-# Add your code here
+# def say_hello(name)
+#   "Hi #{name}!"
+# end
+ 
+# puts "Enter your name:"
+# users_name = gets.chomp
+ 
+# puts say_hello(users_name)
+require "pry"
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help 
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+def list(songs)
+      songs.each_with_index do |item, index| 
+      "#{index + 1}. #{item}"
+    end
+end
+
+# def play(songs)
+#   puts "Please enter a song name or number:"
+# end
+#   user_response = gets.chomp
+# end
+#   if user_response == 
+# puts play(songs)
+
+
+# def exit_jukebox
+# end
+
+# def run 
+# end
